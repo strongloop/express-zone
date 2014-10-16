c76477d892442873daffff02bbe2a6819eec18e9
Bert Belder <bertbelder@gmail.com>
2014-10-16 10:08:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+module.exports = createZoneMiddleware;
+
+var zonesAreEnabled = (global.zone !== undefined);
+
+function createZoneMiddleware() {
+  if (!zonesAreEnabled) {
+    throw new Error('Zones are not available. Ensure that zones are enabled on ' +
+                    'startup.');
+  }
+
+  return wrapRequestInZone;
+}
+
+function wrapRequestInZone(req, res, next) {
+  zone.create(function() {
+    zone.name = req.method + ' ' + req.originalUrl;
+    next();
+
+  }).catch (function(err) {
+    try {
+      res.writeHead('500');
+      res.end('\n' + err.stack);
+    } catch (err) {
+      // Ignore
+    }
+  });
+}
